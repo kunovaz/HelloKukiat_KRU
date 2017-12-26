8d23ddfc31f325d93f634b6bbbe4a2d2b78907ba
Kukiat <Kukiat@gmail.com>
2017-12-26 12:08:26 +0700

    Finish 26Dec

---
+++ app/src/main/java/kukiat/krirk/ac/th/hellokukiat/fragment/MainFragment.java
@@ -0,0 +1,29 @@
+package kukiat.krirk.ac.th.hellokukiat.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import kukiat.krirk.ac.th.hellokukiat.R;
+
+/**
+ * Created by Cnz on 26/12/2017.
+ */
+
+public class MainFragment extends Fragment{
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater,
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+
+        return view;
+    }
+
+} // Main Class
